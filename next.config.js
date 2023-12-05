6135503843a0411a466ad810d1f225871e2cdba8
bruno-ab <brunolasombra@gmail.comb>
2023-12-05 11:10:12 -0300

    fix cors

--- next.config.js
+++ next.config.js
@@ -3,24 +3,14 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/path-to-your-images/:path*", // Substitua pelo caminho real das suas imagens
+        source: "/api/:path", // Substitua pelo caminho real das suas imagens
         headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://static.wikia.nocookie.net",
-          },
-        ],
-      },
-       {
-                // matching all API routes
-                source: "/api/:path",
-                headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
-        }
+      },
     ];
   },
    typescript: {
