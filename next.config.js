59c076fadd8b4b1db073863d8d53407e6c2081e5
bruno-ab <brunolasombra@gmail.comb>
2023-12-05 11:50:29 -0300

    test cors

--- next.config.js
+++ next.config.js
@@ -3,17 +3,26 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/:path", // Substitua pelo caminho real das suas imagens
+        source: "/path-to-your-images/:path*",
         headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://static.wikia.nocookie.net",
+          },
+        ],
+      },
+      {
+        source: "/(.*)", // Correspondendo a todas as rotas
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://cyberpunk-api-262d98a845d6.herokuapp.com" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
       },
     ];
   },
-   typescript: {
+  typescript: {
     ignoreBuildErrors: true,
   },
 };
